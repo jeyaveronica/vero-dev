557b5b96787b534ac99b9bd5bd160a19296007c5
jeyaveronica <114984608+jeyaveronica@users.noreply.github.com>
2023-06-11 16:59:43 +0530

    Exitstatement

---
+++ exampleexit.java
@@ -0,0 +1,20 @@
+package examplebreak;
+
+public class exampleexit {
+	
+public static void main(String[] args) {
+
+	       int n = -2;   
+	    
+	    
+	      if (n < 0) {
+	    	  System.out.println(" It is a negative number");
+	    	  System.exit(0);
+	      }
+	      
+	      System.out.println(n);
+	      }
+	    
+	}
+
+
