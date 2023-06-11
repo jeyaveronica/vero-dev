d08130b596d3292fdfdb23fcedd072ecfdad6996
jeyaveronica <114984608+jeyaveronica@users.noreply.github.com>
2023-06-11 16:48:24 +0530

    Abstractprogramdemo

--- teacher.java
+++ teacher.java
@@ -1,36 +1,30 @@
-package interfacedemo1;
+package abstactdemo1;
 
-interface teacher {
+abstract class teacher {
 	
-	void subject();
-	void passpercentage();
-
-}
-class school implements teacher {
-
-public void subject() {
-	System.out.println("Subject: Maths");
-}
-
-public void passpercentage() {
-	System.out.println("Passpercentage: 80%");
-}
-
-public void result() {
-	System.out.println("Result: 100%");
-}
-
-
-
-public static void main(String[] args) {
-	
-	school ss = new school();
-	ss.passpercentage();
-	ss.subject();
-	ss.result();
-	
+    abstract void student();
+	public void holiday() {
+		System.out.println(" One week holiday");
 	
 }
+}
+
+class head extends teacher {
+	public void student() {
+		System.out.println(" All the best students");
+	}
+}
+
+class school {
+
+	public static void main(String[] args) {
+		head myschool = new head();
+		myschool.student();
+		myschool.holiday();
+		
+		
+	}
 
 }
 
+// if one method is abstract, entire class is abstract. So, mention as "abstract class"
