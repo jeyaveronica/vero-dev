d440e259ad4a4f683f6d1ccf84d3731a5e4b119a
jeyaveronica <114984608+jeyaveronica@users.noreply.github.com>
2023-06-11 16:51:02 +0530

    stringreversal

---
+++ Stringreversalnew.java
@@ -0,0 +1,21 @@
+package stringreversal;
+
+public class Stringreversalnew {
+	
+	public static void main(String[] args) {
+		
+		String a = "abcd";
+		//String b = " ";
+		
+		for(int i = a.length()-1; i >= 0;  i--)
+		{
+			
+			System.out.print(a.charAt(i));
+			
+							
+		}
+		
+		}
+	}
+
+// charAt(i)- Returns the char value at the specified index
