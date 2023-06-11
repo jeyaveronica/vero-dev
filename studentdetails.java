bc9e6a95fe3438bbbd58173b0c09172aee7aa77a
jeyaveronica <114984608+jeyaveronica@users.noreply.github.com>
2023-06-11 16:54:53 +0530

    Studentdetails for encapsulationdemo

---
+++ studentdetails.java
@@ -0,0 +1,33 @@
+package encapsulationdemo;
+
+public class studentdetails {
+	
+	private String studentname;
+	private int age;
+	private int rank;
+	
+	public String getStudentname() {
+		return studentname;
+	}
+	public void setStudentname(String studentname) {
+		this.studentname = studentname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
+	
+	
+	
+	
+
+}
