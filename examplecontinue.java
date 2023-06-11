b576e5a8b901fccfe76f72a5a148003302e5357b
jeyaveronica <114984608+jeyaveronica@users.noreply.github.com>
2023-06-11 16:59:10 +0530

    Continuestatement

---
+++ examplecontinue.java
@@ -0,0 +1,25 @@
+package examplebreak;
+
+public class examplecontinue {
+	
+	
+public static void main(String[] args) {
+
+	          
+	    
+	    for (int i = 50; i >=0; i-=10) {	      
+	     
+	      if (i == 30) {
+	    	   continue;
+	    	  //System.exit(0);
+	    	  //break;
+	      }
+	      
+	      System.out.println(i);
+	      }
+	    
+	}
+	
+	
+
+}
